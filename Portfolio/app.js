14e9e623b1865cbed3de271e6a4d5898bc132630
Aman Kharbanda <amankharbanda2002@gmail.com>
2025-10-05 00:15:05 +0530

    portfolio project completed

---
+++ Portfolio/app.js
@@ -0,0 +1,55 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+document
+  .getElementById("downloadResume")
+  .addEventListener("click", function (e) {
+    e.preventDefault(); // stop default navigation
+
+    const link = document.createElement("a");
+    link.href = "./"; // your resume file path
+    link.download = "Aman-Kharbanda-Software_Engineer.pdf"; // file name when downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+
+document
+  .getElementById("downloadResume2")
+  .addEventListener("click", function (e) {
+    e.preventDefault(); // stop default navigation
+
+    const link = document.createElement("a");
+    link.href = "./"; // your resume file path
+    link.download = "Aman-Kharbanda-Software_Engineer.pdf"; // file name when downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
